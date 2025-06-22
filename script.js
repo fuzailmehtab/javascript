625befc032bb303bb735150192af81d3f4e90bd0
FUZAIL MEHTAB <163311874+fuzailmehtab@users.noreply.github.com>
2025-06-22 11:36:02 +0530

    BMI Calculator

--- script.js
+++ script.js
@@ -1,92 +1,21 @@
-let randomNumber = parseInt(Math.random() * 100 + 1); //generates random number between 1 to 100
+// this is form so we are using submit event
+const form = document.querySelector('form');
 
-const submit = document.querySelector('#subt');
-const userInput = document.querySelector('#guessField');
-const guessSlot = document.querySelector('.guesses');
-const remaining = document.querySelector('.lastResult');
-const lowOrHi = document.querySelector('.lowOrHi');
-const startOver = document.querySelector('.resultParas');
+// this usecase will give us empty,if we use it here outside
+// const height = parseInt(document.querySelector('#height').value;
 
-const p = document.createElement('p');
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  const height = parseInt(document.querySelector('#height').value); //string value ko int me karne ke liye parseInt
+  const weight = parseInt(document.querySelector('#weight').value);
+  const results = document.querySelector('#results');
 
-let prevGuess = []; //jo user submit krega usko store kr lenge taki phirse vahi value repeat na kare
-let numGuess = 1;
-
-let playGame = true;
-if (playGame) {
-  submit.addEventListener('click', function (e) {
-    e.preventDefault();
-    const guess = parseInt(userInput.value);
-    console.log(guess);
-    validateGuess(guess);
-  });
-}
-
-function validateGuess(guess) {
-  //valid guess hai ya nahi
-  if (isNaN(guess)) {
-    alert('please enter a valid number');
-  } else if (guess < 1) {
-    alert('please enter a valid number');
-  } else if (guess > 100) {
-    alert('please enter a valid number');
+  if (height === '' || height < 0 || isNaN(height)) {
+    results.innerHTML = `please enter a valid height ${height} `;
+  } else if (weight === '' || weight < 0 || isNaN(weight)) {
+    results.innerHTML = `please enter a valid height ${weight} `;
   } else {
-    prevGuess.push(guess);
-    // stop game after 10 tries
-    if (numGuess === 11) {
-      displayGuess(guess);
-      displayMessage(`Game over. Random number was ${randomNumber}`);
-      endGame();
-    } else {
-      displayGuess(guess);
-      checkGuess(guess);
-    }
+   const bmi= (weight / ((height * height) / 10000)).toFixed(2); //tofix for 2 decimal number
+    results.innerHTML = `<span>${bmi} </span>`;
   }
-}
-function checkGuess(guess) {
-  if (guess === randomNumber) {
-    displayMessage(`guess is right `);
-    endGame();
-  } else if (guess < randomNumber) {
-    displayMessage(`number is T00 low`);
-  } else if (guess > randomNumber) {
-    displayMessage(`number is T00 high`);
-  }
-}
-function displayGuess(guess) {
-  userInput.value = '';
-  guessSlot.innerHTML += `${guess} `; // guess add kr rhe
-  numGuess++;
-  remaining.innerHTML = `${11 - numGuess}`; //guess remaining hai ye
-}
-function displayMessage(message) {
-  lowOrHi.innerHTML = `<h2> ${message} </h2>`;
-}
-
-function endGame() {
-  // Disables the input box so you can’t guess more.
-  // Adds a “Start new Game” button.
-  // Prepares for the next round.
-  userInput.value = '';
-  userInput.setAttribute('disabled', '');
-  p.classList.add('button'); //paragraph me class button add krdi,paragraph global h
-  p.innerHTML = `<h2 id='newGame'>Start new game</h2>`;
-  // result paras vala pura div lenege ab, usme ab change krenge
-  startOver.appendChild(p);
-  playGame = false;
-  newGame();
-}
-
-function newGame() {
-  const newGameButton = document.querySelector('#newGame');
-  newGameButton.addEventListener('click', function (e) {
-    randomNumber = parseInt(Math.random() * 100 + 1);
-    prevGuess = [];
-    numGuess = 1;
-    guessSlot.innerHTML = '';
-    remaining.innerHTML = `${11 - numGuess}`;
-    userInput.removeAttribute('disabled');
-    startOver.removeChild(p);
-    playGame = true;
-  });
-}
+});
